e038b0adaaabafcf14085a18f803dfec6405e42b
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-02-29 20:29:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Verify
 VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
-HOWTOVERIFY = environ.get('HOWTOVERIFY', url='https://t.me/VenomStoneMovies/2430') # How to open tutorial link for verification
+HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/VenomStoneMovies/2430') # How to open tutorial link for verification
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'publicearn.com')
